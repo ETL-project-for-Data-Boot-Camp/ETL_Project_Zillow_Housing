cd84162524c85c0cf6b19ca19542706af8474a0f
yxc120330 <60168815+yxc120330@users.noreply.github.com>
2020-04-25 11:46:41 -0500

    Add files via upload

---
+++ zillow_housing.sql
@@ -0,0 +1,21 @@
+CREATE TABLE zillow(
+	zip INT,
+	City Varchar(30) not null,
+	State Varchar(30) not null,
+	Latitude Float,
+	Longitude Float,
+	CountyName Varchar(30) not null,
+	"2018-01" INT,
+	"2018-02" INT,
+	"2018-03" INT,
+	"2018-04" INT,
+	"2018-05" INT,
+	"2018-06" INT,
+	"2018-07" INT,
+	"2018-08" INT,
+	"2018-09" INT,
+	"2018-10" INT,
+	"2018-11" INT,
+	"2018-12" INT
+);
+
